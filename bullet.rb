fc3163ccf0fbb814be790238c19ff55bfa2a3a8f
Ryo Enkita <mildgo7925@gmail.com>
2022-03-08 11:44:09 +0900

    add bullet of enemy

--- bullet.rb
+++ bullet.rb
@@ -3,14 +3,22 @@ class Bullet
 
   def initialize(x, y, z)
     @mesh = Mittsu::Mesh.new(
-      Mittsu::BoxGeometry.new(2.0, 2.0, 2.0),
-      Mittsu::MeshBasicMaterial.new(color: 0x00ff00)
+      Mittsu::SphereGeometry.new(2.0, 2.0, 2.0),
+      Mittsu::MeshPhongMaterial.new(color: 0x00ff00)
     )
     @mesh.position.set(x, y, z)
+
+    @mesh2 = Mittsu::Mesh.new(
+      Mittsu::SphereGeometry.new(2.0, 2.0, 2.0),
+      Mittsu::MeshPhongMaterial.new(color: 0xff0000)
+    )
+    @mesh2.position.set(x, y, z)
+
     @speedZ = 0.5
   end
 
   def update
     @mesh.position.z -= @speedZ
+    @mesh2.position.z += @speedZ
   end
 end
