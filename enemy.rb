1127f9510f1042714b28df6a97b19dbdaa968fea
youom <you@example.com>
2022-04-20 12:15:14 +0900

    mod

--- enemy.rb
+++ enemy.rb
@@ -1,8 +1,7 @@
 require_relative 'bullet' #敵の弾はbulletのmesh2を使用
 
 class Enemy
-  attr_accessor :mesh,:bullets
-  attr_reader :flag
+  attr_accessor :mesh,:bullets,:flag
 
   def initialize(x, y, z, renderer, scene)
     @x,@y,@z = x,y,z
@@ -16,23 +15,7 @@ class Enemy
     )
     @mesh.position.set(@x, @y, @z)
 
-    # z軸-30のところにenemy.mesh2をenemy.meshと同じように描画 #
-    @mesh2 = Mittsu::Mesh.new(
-      Mittsu::BoxGeometry.new(1, 1, 1),
-      Mittsu::MeshBasicMaterial.new(color: 0x0000ff)
-    )
-    @mesh2.position.set(@x, @y, @z)
-
-    # z軸-50のところにRuby画像をenemy.mesh3として描画 #
-    geometry = Mittsu::BoxGeometry.new(1,1,1)
-    texture = Mittsu::ImageUtils.load_texture(File.join File.dirname(__FILE__), "texture1.png")
-    material = Mittsu::MeshBasicMaterial.new(map: texture)
-    @ruby = Mittsu::Mesh.new(geometry, material)
-    @ruby.position.set(@x, @y, @z)
-
     @scene.add(@mesh)
-    @scene.add(@mesh2)
-    @scene.add(@ruby)
   end
 
   def fire#敵が弾を発射
@@ -55,27 +38,7 @@ class Enemy
     @flag = 0
   end
 
-  # enemy.mesh3に関しては別にhit, dead関数を作成 #
-
   def update#ランダム移動
-    dx = rand(3)
-		dy = rand(3)
-		case dx
-		when 1
-			self.mesh.position.x += 0.01
-		when 2
-			self.mesh.position.x -= 0.01
-		end
-
-		case dy
-		when 1
-			self.mesh.position.y += 0.01
-		when 2
-			self.mesh.position.y -= 0.01
-		end
-  end
-
-  def update2 
     @mesh.position.x += 3
   end
 end
